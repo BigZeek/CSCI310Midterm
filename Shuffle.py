f78bfcf713ac492ba5e8f24ec006a01d8136da10
BigZeek <92259449+BigZeek@users.noreply.github.com>
2024-10-25 12:52:39 -0500

    Add beginning of csv conversion

--- Shuffle.py
+++ Shuffle.py
@@ -1,5 +1,5 @@
 import os #For looping through directories
-import pandas #for handling csv conversion
+import pandas as pd #for handling csv conversion
 
 #Alternate solution, not currently used
 """ filecount = 0
@@ -14,3 +14,7 @@ print("Number of files converted: ",filecount) """
 for root, dirs, files in os.walk(os.path.abspath("../Midterm/data")):
     for file in files:
         print(os.path.join(root, file))
+        filepath = os.path.join(root, file)
+        #Below code incomplete, will need testing.
+        df = pd.read_csv(file)
+        df.to_csv() # append ".csv" to file name
