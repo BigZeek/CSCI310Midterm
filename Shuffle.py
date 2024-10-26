b77d152a9c9e2180c6cc6ea790d42fe89a6aafb2
BigZeek <92259449+BigZeek@users.noreply.github.com>
2024-10-25 19:29:00 -0500

    Updated Scheduler and walk through files
    Scheduler runs appropriately. Function fetches each directories files in turn. CSV conversion and rename not tested.

--- Shuffle.py
+++ Shuffle.py
@@ -1,40 +1,34 @@
 import os #For looping through directories
 import pandas as pd #for handling csv conversion
-import sched
+import sched #for scheduling repeated run
 import time
-scheduler = sched.scheduler(time.time, time.sleep)
 
-#Alternate solution, not currently used
-""" filecount = 0
-print("Listing txt files:")
-for dirpath, dirnames, filenames in os.walk("."):
-   for filename in filenames:
-      if filename.endswith(".txt"):
-         print(filename)
-         filecount += 1
-print("Number of files converted: ",filecount) """
+scheduler = sched.scheduler(time.time, time.sleep)
 
 def shuffle_files():
    for root, dirs, files in os.walk(os.path.abspath("../Midterm/data")):
-       for file in files:
-           print(os.path.join(root, file))
-           filepath = os.path.join(root, file)
-           #Below code incomplete, will need testing.
-           df = pd.read_csv(file)
-           df.to_csv() # append ".csv" to file name
-        
+       for dir in dirs:
+         count = 0 #used to count files in each directory
+         current_dir = os.path.join(root, dir)
+         print(current_dir) #for troubleshooting, remove when finished
+         
+         file_list = next(os.walk(current_dir))[2] #count only files and ignore subdirectories
+         count += len(file_list)
+         for file in file_list:
+            int_rename = 0
+            print(file) #for troubleshooting, remove when finished
+            
+            #Below code will need testing.
+            df = pd.read_csv(file)
+            df.to_csv(int_rename + '.csv') #rename and convert to csv 
+   print("Shuffle complete!")
+   
 
-        
-        
-        
-        
-        
 
-
-#add function to first scheduler on completion
+#set to run every 5 seconds, set larger gap for video recording.
 def repeat_shuffle():
-   scheduler.enter(15, 1,shuffle_files,())
-   scheduler.enter(15,1,repeat_shuffle, ())
+   scheduler.enter(1,1,shuffle_files,())
+   scheduler.enter(5,1,repeat_shuffle, ())
    
 repeat_shuffle()
-scheduler.run()
+scheduler.run() 
